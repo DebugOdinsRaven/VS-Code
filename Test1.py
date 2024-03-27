491635aee409ab87cfd1338923d299f2f96586a6
Justin Delay <jdelay64@gmail.com>
2024-03-27 16:47:31 -0400

    First Commit

---
+++ Test1.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number between 1 and 10:"))
+if number > 10:
+    print("Your number is out of bounds!")
+elif number < 1:
+    print("Your number is out of bounds!")
+else:
+    print("Thank you for following directions!")
+             
